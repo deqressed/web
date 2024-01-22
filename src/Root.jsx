48a7513ae1c2521925245039e77a6d6b140a02e0
deqressed ~ <notofficialsarah@gmail.com>
2024-01-22 04:17:37 +0300

    Coming Soon...

---
+++ src/Root.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {ParticlesBG,Glow,Content} from './components/'
+
+const Root = () => {
+    return (
+        <>
+            <ParticlesBG />
+            <Glow />
+            <Content />
+        </>
+    )
+}
+
+export default Root
